37f50c6ecd5ed5454475630e47dd3dca5c8288d7
ApricotStyles <jsim247@gmail.com>
2016-01-26 23:21:56 -0600

    Added a class1 test file

---
+++ MasterProject/MasterProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterProject
+{
+    class Class1
+    {
+
+        //this is just a test
+    }
+}
